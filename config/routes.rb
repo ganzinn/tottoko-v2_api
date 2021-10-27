770a50940cf9929c07505b6c2ccb0f985a6be57c
ganzinn <ganzinnpg@gmail.com>
2021-10-28 02:15:25 +0900

    [ADD]users_controller自動生成＆テスト表示用action追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       # api test action
       resources :hello, only: [:index]
+      # users_controller
+      resources :users, only: [:index]
     end
   end
 end
