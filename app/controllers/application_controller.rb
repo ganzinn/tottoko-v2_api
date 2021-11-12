e7a8d559e36ebb3a48a2b4764d522ca515ab78d4
ganzinn <ganzinnpg@gmail.com>
2021-11-12 22:32:22 +0900

    [ADD]共通エラー処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include UserAuth::UserAuthenticate
 
+  private
+
+    def response_401(msg = "認証情報が不正です。")
+      render status: 401, json: { success: false, error: msg }
+    end
+
+    def response_500(msg = "サーバー内でエラーが発生しました。")
+      render status: 500, json: { success: false, error: msg }
+    end
+
 end
