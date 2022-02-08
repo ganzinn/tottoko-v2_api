4e5c53c1799bc36a3a71c864df5e444ae929ecdf
ganzinn <ganzinnpg@gmail.com>
2022-02-08 16:02:30 +0900

    [ADD]バリデーション追加

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -5,4 +5,25 @@ class Creator < ApplicationRecord
   has_many :families, dependent: :destroy
   has_many :users, through: :families
   has_many :works, dependent: :destroy
+
+  # バリデーション -------------------------------------------------
+  validates :name,
+    presence: true,
+    length: {
+      maximum: 40,
+      allow_blank: true
+    }
+
+  validates :date_of_birth,
+    date_format: true
+
+
+  ALLOW_VALUES_GENDER_ID = [1, 2, 3]
+  validates :gender_id,
+    presence: true,
+    inclusion: {
+      in: ALLOW_VALUES_GENDER_ID,
+      allow_blank: true
+    }
+  # ----------------------------------------------------------------
 end
