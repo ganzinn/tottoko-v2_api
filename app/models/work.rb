c104d2c7e743565002fc030007c64a469e0ad62f
ganzinn <ganzinnpg@gmail.com>
2022-02-09 14:51:39 +0900

    [ADD]バリデーション追加

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,26 @@ class Work < ApplicationRecord
   belongs_to_active_hash :scope
 
   belongs_to :creator
+
+  # バリデーション -------------------------------------------------
+  validates :date,
+    date_format: true
+  
+  validates :title,
+    length: { maximum: 40 }
+  
+  validates :description,
+    length: { maximum: 255 }
+
+  ALLOW_VALUES_SCOPE_ID = 1..4
+  validates :scope_id,
+    presence: true,
+    inclusion: {
+      in: ALLOW_VALUES_SCOPE_ID,
+      allow_blank: true
+    }
+
+  validates :creator_id,
+    presence: true
+  # ----------------------------------------------------------------
 end
