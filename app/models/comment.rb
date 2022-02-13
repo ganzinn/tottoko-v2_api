4c646f19aec59c9803dd445ca871c8bc2b03895f
ganzinn <ganzinnpg@gmail.com>
2022-02-13 21:42:00 +0900

    [ADD]コメント機能モデル作成

---
+++ app/models/comment.rb
@@ -0,0 +1,16 @@
+class Comment < ApplicationRecord
+  belongs_to :work
+  belongs_to :user
+
+  # バリデーション -------------------------------------------------
+  validates :message,
+  presence: true
+
+  # ----------------------------------------------------------------
+
+  # コメントの編集・削除権限チェック
+  def edit_permission_check(target_user_id)
+    self.user_id == target_user_id
+  end
+
+end
