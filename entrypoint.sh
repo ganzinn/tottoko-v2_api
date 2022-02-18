9b88fb8ecdf5508e014cd1fd8f25ea6f0f4942a4
ganzinn <ganzinnpg@gmail.com>
2022-02-18 16:26:55 +0900

    [FIX]本番環境マイグレーション処理削除＆デプロイ再設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # 本番DB初期化用(初回のみ)
-#RAILS_ENV=production bin/rails db:create
-
-RAILS_ENV=production bin/rails db:migrate
+# RAILS_ENV=production bin/rails db:create
+# RAILS_ENV=production bin/rails db:migrate
 
 exec "$@"
