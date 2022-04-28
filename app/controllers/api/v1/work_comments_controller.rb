124769a9cf9b39e52e37d86723ad305e7ef18271
ganzinn <ganzinnpg@gmail.com>
2022-04-28 23:40:10 +0900

    [ADD]コメント作成・表示時権限チェック機能追加

--- app/controllers/api/v1/work_comments_controller.rb
+++ app/controllers/api/v1/work_comments_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::WorkCommentsController < ApplicationController
   include Pagination
 
-  before_action :access_token_validate
+  before_action :conditional_access_token_validate, only: [:index]
+  before_action :access_token_validate,             only: [:create]
   before_action :set_work
-  before_action :create_read_permission_check
+  before_action :index_permission_check,            only: [:index]
+  before_action :create_permission_check,           only: [:create]
   
   def create
     comment = Comment.new(create_comment_params)
@@ -17,13 +19,13 @@ class Api::V1::WorkCommentsController < ApplicationController
   def index
     # ページネーション初期化
     page = params[:page] || 1
-    per = params[:per] || 20
+    per = params[:per] || 5
 
-    @comments = Comment.where( work_id: @work.id).order(:created_at).includes(:user).page(page).per(per)
-    if @comments.blank?
-      response_4XX(404, code: "not_found", messages: ['見つかりません']) and return
-    end
-    @current_user_id = authorize_user.id
+    @comments = Comment.where( work_id: @work.id).order(created_at: :DESC).includes(:user).page(page).per(per)
+    # if @comments.blank?
+    #   response_4XX(404, code: "not_found", messages: ['見つかりません']) and return
+    # end
+    @current_user_id = authorize_user ? authorize_user.id : nil
 
     # ページネーション情報の取得
     @pagination = pagination(@comments)
@@ -31,15 +33,32 @@ class Api::V1::WorkCommentsController < ApplicationController
 
   private
 
+  def conditional_access_token_validate
+    access_token_validate if request.headers["Authorization"]&.split&.last
+  end
+
   def set_work
     @work = Work.find(params[:work_id])
     rescue ActiveRecord::RecordNotFound => e
       response_4XX(404, code: "not_found", messages: ['見つかりません'])
   end
 
-  def create_read_permission_check
+  def index_permission_check
+    if authorize_user.present?
+      family = Family.find_by(user_id: authorize_user.id, creator_id: @work.creator_id)
+      unless @work.scope_id == 4 || (family && @work.scope.targets.include?(family.relation_id))
+        response_4XX(401, code: "unauthorized", messages: ['権限がありません'])
+      end
+    else
+      unless @work.scope_id == 4
+        response_4XX(401, code: "unauthorized", messages: ['権限がありません'])
+      end
+    end
+  end
+
+  def create_permission_check
     family = Family.find_by(user_id: authorize_user.id, creator_id: @work.creator_id)
-    unless family && @work.scope.targets.include?(family.relation_id)
+    unless @work.scope_id == 4 || (family && @work.scope.targets.include?(family.relation_id))
       response_4XX(401, code: "unauthorized", messages: ['権限がありません'])
     end
   end
