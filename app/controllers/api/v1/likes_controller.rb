1c29337baea3edfa80ffef5ac141c643f8ef692a
ganzinn <ganzinnpg@gmail.com>
2022-04-28 23:26:48 +0900

    [ADD]追加・削除時いいねカウント表示

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::LikesController < ApplicationController
   def create
     like = Like.new(work_id: params[:work_id], user_id: authorize_user.id)
     if like.save
-      render status: 201, json: { success: true }
+      likes = Like.where(work_id:params[:work_id])
+      render status: 201, json: { success: true, like: {count: likes.size} }
     else
       response_4XX(422, code: "unprocessable", messages: like.errors.full_messages)
     end
@@ -14,7 +15,8 @@ class Api::V1::LikesController < ApplicationController
     like = Like.find_by(work_id:params[:work_id], user_id: authorize_user.id)
     response_4XX(404, code: "not_found", messages: ['見つかりません']) and return if like.blank?
     if like&.destroy
-      render status: 200, json: { success: true }
+      likes = Like.where(work_id:params[:work_id])
+      render status: 200, json: { success: true, like: {count: likes.size} }
     else
       response_4XX(422, code: "unprocessable", messages: @like.errors.full_messages)
     end
